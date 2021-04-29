f302e7f0630da8db8e6bbf9540af9a4d0c5af723
luizv <iam.luizv@gmail.com>
2021-04-29 08:35:50 -0300

    Adiciona a lista 2

---
+++ Exercicios.playground/Pages/2 Funcoes.xcplaygroundpage/Contents.swift
@@ -0,0 +1,107 @@
+import UIKit
+/*:
+ # Lista 2: Funções
+ ### 1. Faça uma função que exiba o quadrado de um número dado. [🐝]
+*/
+// código aqui
+
+/*:
+ ### 2. Crie uma função que exiba o refrão:  [🐝]
+ *"I said, ooh, I'm blinded by the lights No, I can't sleep until I feel your touch I said, ooh, I'm drowning in the night Oh, when I'm like this, you're the one I trust"*
+*/
+// código aqui
+
+/*:
+ ### 3. Faça uma função que receba a idade de uma pessoa em 3 parâmetros: anos, meses e dias. A função deve calcular e exibir a idade da pessoa expressa apenas em dias. Considere todos os anos com 365 dias e todos os meses com 30 dias. [🐝]
+*/
+// código aqui
+
+/*:
+ ### 4. A pontuação de um jogo é a quantia de pontos anteriores de um jogador, somada com os pontos ganhos na última partida. Dado o nome do jogador, a quantia de pontos anteriores e os pontos da última partida, escreva uma função que receba esses parâmetros e mostre os pontos finais dessa pessoa no formato "Fulano: x pontos". [🐝]
+*/
+// código aqui
+
+/*:
+ ### 5. Faça um programa, com uma função que necessite de três argumentos, e que forneça a soma desses três argumentos. [🐝]
+*/
+// código aqui
+
+/*:
+ ### 6. Escreva uma função que receba a largura e altura de um retângulo, calcule e exiba a área e o perímetro desse retângulo. [🐝]
+*/
+// código aqui
+
+/*:
+ ### 7. Faça uma função que retorne o antecessor do número passado pelo parâmetro. [🐝🐝]
+*/
+// código aqui
+
+/*:
+ ### 8. Faça uma função que receba como parâmetro uma medida em metros e converta-a para centímetros. [🐝🐝]
+*/
+// código aqui
+
+/*:
+ ### 9. Crie um programa e uma função que permita fazer a conversão cambial entre Reais e Dólares. A função deve receber como parâmetros: o valor em reais e a taxa cambial. [🐝🐝]
+*/
+// código aqui
+
+/*:
+ ### 10. Escreva uma função que receba como parâmetros a largura e altura de um retângulo, calcule e retorne o comprimento da diagonal desse retângulo. *(Pitágoras)* [🐝🐝]
+*/
+// código aqui
+
+/*:
+ ### 11. Faça uma função que implemente o cálculo do IMC, índice de massa corporal. [🐝🐝]
+*/
+// código aqui
+
+/*:
+ ### 12. Faça um programa que calcule e exiba a média de três notas de um estudante, usando uma função para o cálculo dessa média. [🐝🐝]
+*/
+// código aqui
+
+/*:
+ ### 13. Implemente o cálculo do raio de um círculo para uma dada área. [🐝🐝]
+*/
+// código aqui
+
+/*:
+ ### 14. Faça uma função que retorne a raiz quadrada de um número dado. [🐝🐝]
+ * Callout(Dica):
+ Pesquise a função ````squareRoot()````.
+*/
+// código aqui
+
+/*:
+ ### 15. Faça uma função que retorne o sucessor do número passado como parâmetro. [🐝🐝]
+*/
+// código aqui
+
+/*:
+ ### 16. Escreva uma função que receba como parâmetros: o número total de eleitores de um município e o número de votos nulos. Calcule e retorne o percentual que os votos brancos representam. [🐝🐝]
+*/
+// código aqui
+
+/*:
+ ### 17. De acordo com a ANVISA, a ingestão diária aceitável de mercúrio (Hg) para um ser humano é de 0,1 micrograma/kg de massa corporal. Implemente uma função que receba a massa (em kg) de uma pessoa e retorne a massa (em g) (arredondada para o inteiro mais próximo) de peixe que ela pode ingerir num dia, sabendo que há 0,4 mg de mercúrio/kg do peixe. [🐝🐝🐝]
+*/
+// código aqui
+
+/*:
+ ### 18. Implemente uma função que recebe um número inteiro positivo n e retorne o n-ésimo número da sequência de fibonacci. [🐝🐝🐝🤯]
+* Callout(Atenção):
+A sequência de fibonacci começa em 0; e o retorno se n ≤ 0 deve ser 0.
+*/
+// código aqui
+
+/*:
+ ### 19. Implemente uma função que calcule o produto de todos os números inteiros naturais até um dado número n. [🐝🐝🐝🤯]
+ */
+// código aqui
+
+/*:
+ ### 20. Implemente uma função que calcule a soma de todos os números inteiros naturais até um dado número n. [🐝🐝🐝🤯]
+ */
+// código aqui
+
--- Exercicios.playground/Pages/Menu.xcplaygroundpage/Contents.swift
+++ Exercicios.playground/Pages/Menu.xcplaygroundpage/Contents.swift
@@ -1,5 +1,6 @@
 /*:
   ### Listas de Exercícios
  
- 1. [Constantes e Variáveis](1%20Constantes%20e%20Variaveis) [Disponível]
+ 1. [Constantes e Variáveis](1%20Constantes%20e%20Variaveis)
+ 2. [Funções](2%20Funcoes)
  */
